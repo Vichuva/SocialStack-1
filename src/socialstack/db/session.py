7c4341474f0cb8a270a3646f403a742df4ef40f8
Vichuva <viswanath2k5@gmail.com>
2026-06-16 15:50:43 +0530

    fix: backend bug fix

--- src/socialstack/db/session.py
+++ src/socialstack/db/session.py
@@ -18,6 +18,7 @@ def get_engine():
             pool_pre_ping=True,
             pool_size=10,
             max_overflow=20,
+            connect_args={"statement_cache_size": 0},
         )
     return _engine
 
